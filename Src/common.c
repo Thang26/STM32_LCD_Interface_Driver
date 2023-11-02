bd040eb01c0e77b8a323cb4b67cd4928adcdf968
Thang Pham <thang10262001@gmail.com>
2023-11-02 05:24:02 -0400

    Broken function, temporary add.

--- Src/common.c
+++ Src/common.c
@@ -6,6 +6,7 @@ void USER_LED_INIT(void){
     RCC_GPIO_CLK_ENABLE(GPIOA_CLK_EN);
     GPIOA_MODER_SEL(PIN_5, OUTPUT_SEL);
     GPIOA_BSRR_SEL(PIN_5, BSRR_PIN_CLEAR);
+    USER_LED_CLEAR();
 
 }
 void USER_LED_SET(void){
@@ -18,8 +19,17 @@ void USER_LED_CLEAR(void){
     GPIOA_BSRR_SEL(PIN_5, BSRR_PIN_CLEAR);
 }
 
-void USER_LED_FLASH(void){
+//Broken function
+//TODO: MUST DECLARE FUNCTION PROTOTYPE!!!
+//TODO: MUST REMEMBER TO CLEAR UIE FLAG!!!
 
+// void USER_LED_FLASH(void){
 
+//     USER_LED_INIT();
+//     LED_FLASH_TIMER_INIT();
+//     if((TIM3->SR & (1U << 0))){
+//         TOGGLE_BIT(GPIOA->ODR, 1U << PIN_5);
+//     }
     
-}
+//     else{}
+// }
