8cd70124af3e29ce52cb286a72e7754bc5ee27e4
Thang Pham <thang10262001@gmail.com>
2023-11-02 05:24:26 -0400

    Added a timer init function with the purpose of flashing the user LED.

--- Src/timer_init.c
+++ Src/timer_init.c
@@ -1,1 +1,12 @@
 #include <common.h>
+#include <timer_init.h>
+
+void LED_FLASH_TIMER_INIT(void){
+
+    APB1_TIMERx_ENABLE(TIMER_3_EN);
+    TIM3->PSC = 1600 - 1;
+    TIM3->ARR = 5000 - 1;
+    TIM3->CNT = 0;
+    TIM3->CR1 = 1;
+
+}
