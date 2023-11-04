3e6c6a8fbb68dfe45d1f6c9bbe6772f801e5a71d
Thang Pham <thang10262001@gmail.com>
2023-11-04 03:47:21 -0400

    Further fleshed out the timer init function to flash an LED by configuring it to trigger an interrupt, as well as using the newly defined macros for better understandability.

--- Src/timer_init.c
+++ Src/timer_init.c
@@ -1,12 +1,29 @@
 #include <common.h>
 #include <timer_init.h>
 
-void LED_FLASH_TIMER_INIT(void){
+/*
+ *  Timer init function used by the other LED's flashing functions.
+ *  Default SYSCLK is 62.5ns * (PSC) * (ARR)
+ */
+void LED_FLASH_TIMER_INIT(UINT16 PSC_Div_Val, UINT16 ARR_Div_Val){
 
-    APB1_TIMERx_ENABLE(TIMER_3_EN);
-    TIM3->PSC = 1600 - 1;
-    TIM3->ARR = 5000 - 1;
-    TIM3->CNT = 0;
-    TIM3->CR1 = 1;
+    /* Configs timing parameters for the timer */
+    APB1_TIMERx_ENABLE(TIMER_3_EN); //Enables clock for timer.
+    TIM3->PSC = PSC_Div_Val - 1;
+    TIM3->ARR = ARR_Div_Val - 1;
+    TIM3->CNT = 0;                  //Initially clears the COUNTER within timer.
+    TIM3_FLAG_CLEAR;                //Ensures UIE flag for TIM3 is cleared upon init.
+    
+    /* Configs IRQ for the timer */
+    TIM3_IRQ_EN;                    //Configs TIM3 to trigger IRQ upon UIE event.
+    NVIC_EnableIRQ(TIM3_IRQn);      //Enable IRQ in NVIC
+
+    /* Set the timer in motion */
+    TIM3_START;                     //Enables the timer.
 
 }
+
+/*
+ *  500ms       PSC = 1600, ARR = 5000
+ *  1000ms      PSC = 1600, ARR = 10000
+ */
