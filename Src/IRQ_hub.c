27456be85c20b7032d7d9c9bd0df702ea24bd1b4
Thang Pham <thang10262001@gmail.com>
2023-11-04 03:46:01 -0400

    Created a c file where all the interrupt handler functions live in.

---
+++ Src/IRQ_hub.c
@@ -0,0 +1,20 @@
+/*
+ * IRQ_hub.c
+ *
+ *  Created on: Nov 04, 2023
+ *      Author: Thang Pham
+ * 
+ *  This is a collection of all the IRQ service routine functions.
+ *  
+ *  IMPORTANT: You must abstract whatever functionalities associated
+ *  with the IRQ away from the handler itself. This is to foster the idea
+ *  that an IRQ handler can be swapped in and out the functionalities
+ *  associated with that IRQ.
+ * 
+ *  This can be done by having the IRQ envoke a function. EXPERIMENTAL.
+ */
+
+void TIM2_IRQHandler(void){
+
+    
+}
