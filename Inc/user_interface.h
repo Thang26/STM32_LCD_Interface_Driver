2c9ccd91cf46966f2e8b24410a6aef210107e7c8
Thang Pham <thang10262001@gmail.com>
2023-11-04 05:07:50 -0400

    Added function prototype declarations for the functions of user_interface.c

---
+++ Inc/user_interface.h
@@ -0,0 +1,20 @@
+/*
+ * user_interface.h
+ *
+ *  Created on: Nov 04, 2023
+ *      Author: Thang Pham
+ * 
+ *  Header file associated with the user_interface.c
+ *  
+ */
+
+/*
+ *  Function prototype declaration
+ */
+
+void USER_LED_CLEAR(void);
+void USER_LED_SET(void);
+void USER_LED_TOGGLE(void);
+void USER_LED_INIT(void);
+void USER_LED_SLOW_BLINK(void);
+void USER_LED_FAST_BLINK(void);
