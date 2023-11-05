4a954de4db8762595ff95fb1d47e1fba26f88af0
Thang Pham <thang10262001@gmail.com>
2023-11-04 23:14:06 -0400

    Creation of a header file associated with lcd_init.c

---
+++ Inc/lcd_init.h
@@ -0,0 +1,13 @@
+/*
+ * lcd_init.h
+ *
+ *  Created on: Nov 04, 2023
+ *      Author: Thang Pham
+ */
+
+/*
+ *  Function Prototype Declarations
+ */
+
+OPERATION_RESULT LCD_PORT_INIT(void);
+OPERATION_RESULT LCD_STARTUP(void);
