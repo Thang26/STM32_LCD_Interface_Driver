3a40c0c3734274288ac7dcf0f2add4c1fc4aafc1
Thang Pham <thang10262001@gmail.com>
2023-11-04 23:20:34 -0400

    Added a function that will append the timer delay during runtime.

--- Src/timer_init.c
+++ Src/timer_init.c
@@ -13,7 +13,8 @@ void LED_FLASH_TIMER_INIT(UINT16 PSC_Div_Val, UINT16 ARR_Div_Val){
     TIM3->ARR = ARR_Div_Val - 1;
     TIM3->CNT = 0;                  //Initially clears the COUNTER within timer.
     TIM3_FLAG_CLEAR;                //Ensures UIE flag for TIM3 is cleared upon init.
-    
+    TIM3->CR1 |= (1U << 7);         //Turns on ARR Preload Buffer
+
     /* Configs IRQ for the timer */
     TIM3_IRQ_EN;                    //Configs TIM3 to trigger IRQ upon UIE event.
     NVIC_EnableIRQ(TIM3_IRQn);      //Enable IRQ in NVIC
@@ -23,8 +24,19 @@ void LED_FLASH_TIMER_INIT(UINT16 PSC_Div_Val, UINT16 ARR_Div_Val){
 
 }
 
+void LED_FLASH_TIMER_UPDATE_VAL(UINT16 ARR_Div_Val){
+
+    TIM3->ARR = ARR_Div_Val - 1;
+
+}
+
 /*
  *  250ms       PSC = 1600, ARR = 2500
  *  500ms       PSC = 1600, ARR = 5000
  *  1000ms      PSC = 1600, ARR = 10000
+ */
+
+/*
+ * ARR Preload Buffer ensures that any updates to the ARR register
+ * will be applied AFTER the current timer cycle has finished.
  */
