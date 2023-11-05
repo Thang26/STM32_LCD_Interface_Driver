ddac92268a7f06947286a7b41f86b08a44490332
Thang Pham <thang10262001@gmail.com>
2023-11-04 23:21:22 -0400

    Created user LED blinking functions (fast and slow).

--- Src/user_interface.c
+++ Src/user_interface.c
@@ -12,13 +12,10 @@
 #include <port_init.h>
 #include <timer_init.h>
 
-void USER_LED_INIT(void){
+/*
+ *  Functions to interface with the onboard LED (GPIOA Pin 5)
+ */
 
-    RCC_GPIO_CLK_ENABLE(GPIOA_CLK_EN);
-    GPIOA_MODER_SEL(PIN_5, OUTPUT_SEL);
-    USER_LED_CLEAR();
-
-}
 void USER_LED_SET(void){
 
     GPIOA_BSRR_SEL(PIN_5, BSRR_PIN_SET);
@@ -34,28 +31,25 @@ void USER_LED_TOGGLE(void){
     GPIOA_TOGGLE_PIN(PIN_5);
 }
 
-void USER_LED_SLOW_BLINK(void){
+void USER_LED_INIT(void){
+
+    RCC_GPIO_CLK_ENABLE(GPIOA_CLK_EN);
+    GPIOA_MODER_SEL(PIN_5, OUTPUT_SEL);
+    USER_LED_CLEAR();
+}
+
+void USER_LED_BLINK_INIT(void){
 
     USER_LED_INIT();
-    LED_FLASH_TIMER_INIT(1600, 10000);
+    LED_FLASH_TIMER_INIT(1600, 10000);  //Sets default to blink at 1 Hz
+}
+
+void USER_LED_SLOW_BLINK(void){
+
+    LED_FLASH_TIMER_UPDATE_VAL(10000);
 }
 
 void USER_LED_FAST_BLINK(void){
 
-    //TODO
+    LED_FLASH_TIMER_UPDATE_VAL(2500);
 }
-
-//Broken function
-//TODO: MUST DECLARE FUNCTION PROTOTYPE!!!
-//TODO: MUST REMEMBER TO CLEAR UIE FLAG!!!
-
-// void USER_LED_FLASH(void){
-
-//     USER_LED_INIT();
-//     LED_FLASH_TIMER_INIT();
-//     if((TIM3->SR & (1U << 0))){
-//         TOGGLE_BIT(GPIOA->ODR, 1U << PIN_5);
-//     }
-    
-//     else{}
-// }
