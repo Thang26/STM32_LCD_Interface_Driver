3bed591dc28fcdbc25473ef680b97f1341c715fb
Thang Pham <thang10262001@gmail.com>
2023-11-06 00:44:23 -0500

    Created a mask for BSRR macro.

--- Inc/user_interface.h
+++ Inc/user_interface.h
@@ -19,3 +19,12 @@ void USER_LED_INIT(void);
 void USER_LED_BLINK_INIT(void);
 void USER_LED_SLOW_BLINK(void);
 void USER_LED_FAST_BLINK(void);
+
+/*
+ *  Macro for User LED Interface
+ */
+
+#define USER_LED_ON    0
+#define USER_LED_OFF  16
+#define USER_LED_CONFIG(PIN_NUM, PIN_SET_CLEAR)    GPIOA_BSRR_SEL(PIN_NUM, PIN_SET_CLEAR)
+#define USER_LED_CONFIG_TOGGLE(PIN_NUM)            GPIOA_TOGGLE_PIN(PIN_NUM)
