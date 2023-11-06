113c95c1f49ef3e24451a9c5b4dbbc06b7bd4afe
Thang Pham <thang10262001@gmail.com>
2023-11-06 00:45:04 -0500

    Added a second timer for any delays and timings related to the LCD interfacing.

--- Src/timer_init.c
+++ Src/timer_init.c
@@ -27,10 +27,11 @@ void LED_FLASH_TIMER_INIT(UINT16 PSC_Div_Val, UINT16 ARR_Div_Val){
 void LED_FLASH_TIMER_UPDATE_VAL(UINT16 ARR_Div_Val){
 
     TIM3->ARR = ARR_Div_Val - 1;
-
 }
 
 /*
+ *  LED FLASH TIMER VALUES
+ *
  *  250ms       PSC = 1600, ARR = 2500
  *  500ms       PSC = 1600, ARR = 5000
  *  1000ms      PSC = 1600, ARR = 10000
@@ -39,4 +40,31 @@ void LED_FLASH_TIMER_UPDATE_VAL(UINT16 ARR_Div_Val){
 /*
  * ARR Preload Buffer ensures that any updates to the ARR register
  * will be applied AFTER the current timer cycle has finished.
+ */
+
+void LCD_DELAY_OPM_TIMER_INIT(UINT16 ARR_Div_Val){
+
+    APB1_TIMERx_ENABLE(TIMER_2_EN);
+    TIM2->PSC = 1600 - 1;
+    TIM2->ARR = ARR_Div_Val - 1;
+    TIM2->CNT = 0;
+    TIM2_OPM_EN;
+    TIM2_FLAG_CLEAR;
+    TIM2_START;
+}
+
+void LCD_DELAY_OPM_TIMER_UPDATE(UINT16 ARR_Div_Val){
+
+    TIM2_FLAG_CLEAR;
+    TIM2->CNT = 0;
+    TIM2->ARR = ARR_Div_Val - 1;
+    TIM2_START;
+}
+
+/*
+ *  LCD DELAY TIMER VALUES
+ *
+ *  30ms       PSC = 1600, ARR = 300
+ *  10ms       PSC = 1600, ARR = 100
+ *  1ms        PSC = 1600, ARR = 10
  */
